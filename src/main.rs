973b72603c56620d8f8c0b9ba1a5452d5a2e23ad
Félix Saparelli <felix@passcod.name>
2024-11-25 21:43:19 +1300

    feat: get connection and outdir from args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{error::Error, time::Duration};
+use std::{error::Error, path::PathBuf, time::Duration};
 
+use clap::{CommandFactory, Parser, ValueHint};
+use pg_connection_string::ConnectionString;
 use pg_replicate::pipeline::{
     batching::{data_pipeline::BatchDataPipeline, BatchConfig},
     sources::postgres::{PostgresSource, TableNamesFrom},
@@ -9,6 +11,26 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt as _};
 
 mod audit_sink;
 mod postgres_sink;
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Args {
+    /// Connection string to Postgres in libpq format
+    #[arg(short = 'P', long, value_name = "POSTGRES_CONNSTRING")]
+    pg: ConnectionString,
+
+    /// Output directory
+    #[arg(short, long = "out", value_name = "PATH", value_hint = ValueHint::DirPath)]
+    out_dir: PathBuf,
+
+    /// Maximum batch size
+    #[arg(long, default_value = "100")]
+    max_batch_size: usize,
+
+    /// Batch timeout in seconds
+    #[arg(long, default_value = "10")]
+    max_batch_time: u64,
+}
 
 #[tokio::main]
 
@@ -21,20 +43,50 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let args = Args::parse();
+
     let source = PostgresSource::new(
-        "/var/run/postgresql",
-        5432,
-        "prepper",
-        "passcod",
-        None,
-        Some("prepper_slot".into()),
-        TableNamesFrom::Publication("prepper_pub".into()),
+        &args.pg.hostspecs[0].host.to_string(),
+        args.pg.hostspecs[0].port.unwrap_or(5432),
+        &args.pg.database.unwrap_or_default(),
+        &args.pg.user.unwrap_or_default(),
+        args.pg.password,
+        args.pg.parameters.iter().find_map(|p| {
+            if p.keyword == "slot" {
+                Some(p.value.clone())
+            } else {
+                None
+            }
+        }),
+        TableNamesFrom::Publication(
+            args.pg
+                .parameters
+                .iter()
+                .find_map(|p| {
+                    if p.keyword == "pub" {
+                        Some(p.value.clone())
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or_else(|| {
+                    Args::command()
+                        .error(
+                            clap::error::ErrorKind::InvalidValue,
+                            "missing parameter pub",
+                        )
+                        .exit()
+                }),
+        ),
     )
     .await?;
 
-    let sink = audit_sink::AuditSink::new("history".into());
+    let sink = audit_sink::AuditSink::new(args.out_dir);
 
-    let batch_config = BatchConfig::new(100, Duration::from_secs(10));
+    let batch_config = BatchConfig::new(
+        args.max_batch_size,
+        Duration::from_secs(args.max_batch_time),
+    );
     let mut pipeline = BatchDataPipeline::new(source, sink, PipelineAction::Both, batch_config);
 
     pipeline.start().await?;
